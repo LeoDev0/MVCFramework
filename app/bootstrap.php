4b59241a45ddbc3ea7986955d5d7f07aefdf430e
Leonardo T. Fernandes <leo.80@protonmail.com>
2021-01-11 08:57:05 -0300

    set up bootstrap file for URL parameters

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,5 @@
-hello boots
+<?php
+
+require_once 'libraries/Core.php';
+require_once 'libraries/Controller.php';
+require_once 'libraries/Database.php';
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once '../app/bootstrap.php';
+
+// Init core Library
+
+$init = new Core;
