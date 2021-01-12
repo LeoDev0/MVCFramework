b9a3987f7ca4d14db49b1a0b90a7c00ec30215d2
Leonardo T. Fernandes <leo.80@protonmail.com>
2021-01-11 21:17:25 -0300

    add load controllers from URL

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -12,10 +12,31 @@ class Core {
     protected $params = [];
 
     public function __construct() {
-        $this->getUrl();
+
+        if ($this->getUrl() !== null) {
+            $url = $this->getUrl();
+
+            // Look in controllers if there's one created
+            if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+                // If exists, set as controller
+                $this->currentController = ucwords($url[0]);
+                unset($url[0]);
+            }
+        }
+        
+        // Require the controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        $this->currentController = new $this->currentController;
     }
 
     public function getUrl() {
-        echo $_GET['url'];
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+
+            return $url;
+        }
     } 
 }
