58429a4fe8186e338b0b0c1d4d2d6821997b0522
Leonardo T. Fernandes <leo.80@protonmail.com>
2021-01-13 08:29:31 -0300

    add config file and autoloader

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// App root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL root
+define('URLROOT', 'http://localhost/MVCFramework');
+
+// Site name
+define('SITENAME', 'MVCFramework');
